da77c20bf9b9c662fe36e9a3ffc1afbe55887d12
KonH <konh@yandex.ru>
2020-06-04 22:33:54 +0700

    Add sample test

---
+++ Assets/Tests/ListTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Unity.PerformanceTesting;
+
+namespace Tests {
+	public sealed class ListTests {
+		[Test, Performance]
+		public void CreateAndFill() {
+			Measure
+				.Method(() => {
+					var list = new List<string>();
+					for (var i = 0; i < 100; i++) {
+						list.Add("");
+					}
+				})
+				.GC()
+				.Run();
+		}
+	}
+}
